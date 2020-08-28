881b956d43c76749c094ac4c7c56db6d597d93f3
Diana <dianahdezsoler@gmail.com>
2020-08-28 09:26:54 +0200

    performance updated

--- _layout/default.js
+++ _layout/default.js
@@ -1,14 +1,16 @@
-import Header from "@includes/header";
-import { styles } from "@layout/styles";
+import Header from "@includes/header"
+import { styles } from "@layout/styles"
 
 export default function DefaultLayout(props) {
   return (
-    <main>
-      <Header />
-      {props.children}
-      <style jsx global>
-        {styles}
-      </style>
-    </main>
-  );
+    <html lang="es">
+      <main>
+        <Header />
+        {props.children}
+        <style jsx global>
+          {styles}
+        </style>
+      </main>
+    </html>
+  )
 }
