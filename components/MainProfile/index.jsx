c815b472a602a51d9b0022d3f9fe7e127dba685b
Diana Hernández <dianahdezsoler@gmail.com>
2021-06-26 10:33:21 +0200

    name's color updated

--- components/MainProfile/index.jsx
+++ components/MainProfile/index.jsx
@@ -7,7 +7,7 @@ export default function MainProfile() {
         <Image src="./img/main/hulk.png" />
       </BackgroundImage>
       <Text>
-        <h1 style={{marginBottom: "-1rem"} }>¡Hola, soy Diana!</h1>
+        <h1 style={{ marginBottom: "-1rem" }}>¡Hola, soy <span style={{color: "#218d7e"}}>Diana</span>!</h1>
         <br />
         👩‍💻 | 🖖 | 🏳️‍🌈 | 📸 | 📺 | ☕ | ✈️ <br /> <div style={{textAlign: "left", marginTop: ".6rem"}}>Desarrolladora mobile junior. Friki
         de nacimiento, me gusta la fotografía, cualquier cosa con canela y reir a carcajadas.</div>
